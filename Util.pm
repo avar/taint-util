d7de1f9769302433211d12144b8b6104a7401d01
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-03-13 12:49:32 +0000

    update copyright year to 2010

--- Util.pm
+++ Util.pm
@@ -110,7 +110,7 @@ E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
 
 =head1 LICENSE
 
-Copyright 2007-2008 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason.
+Copyright 2007-2010 E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
